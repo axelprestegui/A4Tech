b31d7bb152ea87ea899ed8139c3943f2145879c6
DerekAlmanza <Derek.Almanza@ciencias.unam.mx>
2021-08-04 16:41:48 -0500

    Agregado a la vista el valor cantidad en la compra del producto y vista de evalúa producto realizada.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from config import Config
 from routes.producto_bp import producto_bp
 from routes.usuario_bp import usuario_bp
+from routes.evaluacion_bp import evaluacion_bp
 from models.Modelos import db
 
 
@@ -14,6 +15,8 @@ db.init_app(app)
 app.register_blueprint(producto_bp, url_prefix='/producto')
 
 app.register_blueprint(usuario_bp, url_prefix='/usuario')
+
+app.register_blueprint(evaluacion_bp, url_prefix='/evaluacion')
 
 app.config.update(
     TESTING=True,
---
+++ controllers/EvaluaController.py
@@ -0,0 +1,20 @@
+from flask import render_template, redirect, url_for, request, abort, jsonify
+from models.Modelos import Producto,ProductoEsquema, Compra, CompraEsquema
+from flask_sqlalchemy import SQLAlchemy
+from pathlib import Path
+from werkzeug.utils import secure_filename
+import os.path
+import sys
+import shutil
+
+db = SQLAlchemy() # nuestro ORM
+
+imagenes_validas = [".jpg",".gif",".png",".jpeg"] # las formatos de imágenes que permitimos
+
+"""
+Método que dará acceso a '.../producto/compra_producto' la cual será una vista encargada de recabar 
+la información necesaria para llevar a cabo la compra del producto.
+"""
+def evaluar_producto():
+    if request.method != 'POST':
+        return render_template('evaluacion/evaluacion_producto.html')
